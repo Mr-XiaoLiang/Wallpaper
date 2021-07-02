9ea83816bda8496091edbd763d6d1599b2a4db68
lollipop <liujian594@hotmail.com>
2021-07-02 22:32:58 +0800

    推进颜色生成activity

--- app/src/main/java/com/lollipop/wallpaper/activity/GenerateActivity.kt
+++ app/src/main/java/com/lollipop/wallpaper/activity/GenerateActivity.kt
@@ -1,7 +1,12 @@
 package com.lollipop.wallpaper.activity
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.lollipop.wallpaper.databinding.ActivityGenerateBinding
+import com.lollipop.wallpaper.databinding.ItemSelectColorBinding
+import com.lollipop.wallpaper.utils.bind
 import com.lollipop.wallpaper.utils.lazyBind
 
 /**
@@ -17,4 +22,32 @@ class GenerateActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding)
     }
+
+    private class ColorHolder(
+        private val binding: ItemSelectColorBinding,
+        private val onClick: (position: Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun create(parent: ViewGroup, onClick: (position: Int) -> Unit): ColorHolder {
+                return ColorHolder(parent.bind(), onClick)
+            }
+        }
+
+        private val colorDrawable = ColorDrawable()
+
+        init {
+            binding.colorPreviewView.setImageDrawable(colorDrawable)
+            itemView.setOnClickListener {
+                onClick(adapterPosition)
+            }
+        }
+
+        fun bind(color: Int, isSelected: Boolean) {
+            binding.colorPreviewView.isSelected = isSelected
+            colorDrawable.color = color
+        }
+
+    }
+
 }
